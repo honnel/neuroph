4e6b1d4d48883664fe63e895f09c9d8814e5c756
Multikern-Praktikum WS2012-2013 <mk03@i41pc147.ipd.uni-karlsruhe.de>
2013-02-04 22:10:40 +0100

    Mit 8 Threads

--- eval/cernEval/cernEval.sh
+++ eval/cernEval/cernEval.sh
@@ -1,2 +1,3 @@
-java -jar ../runexperiment.jar -cf experiment_configs/dataSet-15000_threads-4.txt -o results/ -v -csv -i -d
-java -jar ../runexperiment.jar -cf experiment_configs/testGenetic.txt -o results/ -v -csv -i -d
+java -jar ../runexperiment.jar -cf experiment_configs/dataSet-15000_threads-4.txt -o results/ -csv -i -d
+java -jar ../runexperiment.jar -cf experiment_configs/testGenetic.txt -o results/ -csv -i -d
+java -jar ../runexperiment.jar -cf experiment_configs/dataSet-15000_threads-8.txt -o results/ -csv -i -d
